e9dced2fab166f1a984326a028234d9c5701f1d2
muhammadhaqqi <muhammad.haqqi01@ui.ac.id>
2025-08-01 22:54:22 +0700

    chore: add dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// https://dev.to/boostup/uncaught-referenceerror-process-is-not-defined-12kg
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.REACT_APP_BE_URL_BASE_LOCAL": JSON.stringify(
+        env.REACT_APP_BE_URL_BASE_LOCAL
+      ),
+    },
+    plugins: [react()],
+  };
+});
