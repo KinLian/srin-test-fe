18d61d0af2e7012822a8df81d65ca31a61a0c896
muhammadhaqqi <muhammad.haqqi01@ui.ac.id>
2025-08-02 02:41:24 +0700

    feat: add main page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,36 @@
+import { Button } from "antd";
+import { MainLayout } from "../../components/layout";
+import { useNavigate } from "react-router";
+
 function HomePage() {
-  return <p>Home Page</p>;
+  const navigate = useNavigate();
+
+  return (
+    <MainLayout
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <h1>SRIN Take Home Test</h1>
+        <h2>Electronics Catalogue</h2>
+      </div>
+      <div style={{ marginTop: "4rem" }}>
+        <Button
+          variant="filled"
+          color="blue"
+          style={{ fontSize: "2rem", padding: "4rem", width: "fit-content" }}
+          onClick={() => navigate("/phone")}
+        >
+          Phones
+        </Button>
+      </div>
+    </MainLayout>
+  );
 }
 
 export default HomePage;
