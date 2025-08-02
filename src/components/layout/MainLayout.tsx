65a4589f22518dab85c4d08cf8cb4432d40428f8
muhammadhaqqi <muhammad.haqqi01@ui.ac.id>
2025-08-02 11:17:40 +0700

    feat: add loading for mainlayout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,39 +1,43 @@
 import type { CSSProperties, ReactNode } from "react";
 import { colors } from "../../styles";
-import { Flex, type FlexProps } from "antd";
+import { Flex, Spin, type FlexProps } from "antd";
 
 interface MainLayoutProps extends FlexProps {
-  style?: CSSProperties;
+  style?: Omit<CSSProperties, "padding">;
+  isLoading?: boolean;
   children: ReactNode;
 }
 
 const defaultStyle: CSSProperties = {
   background: colors["gray-10"],
   padding: "2rem",
-  minWidth: "calc(100vw - 4rem)",
-  minHeight: "calc(100vh - 4rem)",
+  boxSizing: "border-box",
+  maxWidth: "100vw",
+  minHeight: "98vh",
 };
 
 const innerStyle: CSSProperties = {
   background: "white",
   borderRadius: "0.5rem",
-  paddingInline: "2rem",
-  paddingBlock: "2rem",
-  minHeight: "calc(100vh - 8rem)",
+  padding: "2rem",
+  boxSizing: "border-box",
+  width: "calc(100vw - 5rem)",
+  minHeight: "calc(100vh - 5rem)",
 };
 
 function MainLayout({
   children,
   style,
   vertical = true,
+  isLoading = true,
   ...props
 }: MainLayoutProps) {
   return (
-    <div style={defaultStyle}>
+    <Flex style={defaultStyle}>
       <Flex vertical={vertical} {...props} style={{ ...style, ...innerStyle }}>
-        {children}
+        {isLoading ? children : <Spin fullscreen size="large" />}
       </Flex>
-    </div>
+    </Flex>
   );
 }
 
